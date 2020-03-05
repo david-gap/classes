ee1b7392432167b064923053126efc6d9ef0d8b1
david-gap <david.voglgsang@gmail.com>
2020-03-05 13:13:23 +0100

    📦 NEW: WPsearch class

---
+++ classes/prefix_WPsearch/class.bd_WPsearch.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * WP ADDON FOR ACF
+ * https://github.com/david-gap/classes
+ *
+ * @author      David Voglgsang
+ * @version     1.0
+ *
+*/
+
+/*=======================================================
+Table of Contents:
+---------------------------------------------------------
+1.0 INIT & VARS
+  1.1 CONFIGURATION
+  1.2 ON LOAD RUN
+  1.3 BACKEND ARRAY
+2.0 FUNCTIONS
+  2.1 GET SETTINGS FROM CONFIGURATION FILE
+  2.2 TAXONOMIES SEARCH
+  2.3 ACF SEARCH
+3.0 OUTPUT
+=======================================================*/
+
+
+class prefix_WPsearch extends prefix_core_BaseFunctions {
+
+  /*==================================================================================
+    1.0 INIT & VARS
+  ==================================================================================*/
+
+    /* 1.1 CONFIGURATION
+    /------------------------*/
+    /**
+      * default vars
+      * @param static array $WPsearch_acf: list of advanced custom fields you want to search content in
+      * @param static array $WPsearch_tax: list of taxonomies you want to search content in
+    */
+    static $WPsearch_acf = array();
+    static $WPsearch_tax = array();
+
+
+    /* 1.2 ON LOAD RUN
+    /------------------------*/
+    public function __construct() {
+      // update default vars with configuration file
+      SELF::updateVars();
+      // add acf fields to search query
+      if(!empty(is_array(SELF::$WPsearch_acf))):
+        add_filter( 'posts_search', array( $this, 'WPsearch_CustomSearch'), 500, 2 );
+      endif;
+      // add taxonomies search query
+      if(!empty(is_array(SELF::$WPsearch_tax))):
+        add_action('parse_query', array( $this, 'WPsearch_Taxonomies'), 1 );
+      endif;
+    }
+
+    /* 1.3 BACKEND ARRAY
+    /------------------------*/
+    static $backend = array(
+      "key" => array(
+        "label" => "",
+        "type" => "",
+        "value" => ""
+      ),
+    );
+
+
+
+  /*==================================================================================
+    2.0 FUNCTIONS
+  ==================================================================================*/
+
+
+  /* 2.1 GET SETTINGS FROM CONFIGURATION FILE
+  /------------------------*/
+  private function updateVars(){
+    // get configuration
+    global $configuration;
+    // if configuration file exists && class-settings
+    if($configuration && array_key_exists('WPsearch', $configuration)):
+      // class configuration
+      $myConfig = $configuration['WPsearch'];
+      // update vars
+      SELF::$WPsearch_acf = array_key_exists('acf', $myConfig) ? $myConfig['acf'] : SELF::$WPsearch_acf;
+      SELF::$WPsearch_tax = array_key_exists('taxonomies', $myConfig) ? $myConfig['taxonomies'] : SELF::$WPsearch_tax;
+    endif;
+  }
+
+
+  /* 2.2 TAXONOMIES SEARCH
+  /------------------------*/
+  public function WPsearch_Taxonomies(&$query)
+  {
+    if ($query->is_search)
+    foreach (SELF::$WPsearch_tax as $key => $value) {
+      $query->set('taxonomy', $value);
+    }
+  }
+
+  /* 2.3 ACF SEARCH
+  /------------------------*/
+  public function WPsearch_CustomSearch( $where, $wp_query ) {
+    global $wpdb;
+    // fallback if target is missing
+    if ( empty( $where )):
+      return $where;
+    endif;
+    // get search expression
+    $terms = $wp_query->query_vars[ 's' ];
+    // explode search expression to get search terms
+    $exploded = explode( ' ', $terms );
+    if( $exploded === FALSE || count( $exploded ) == 0 )
+        $exploded = array( 0 => $terms );
+    // reset search in order to rebuilt it as we whish
+    $where = '';
+    // get searcheable acf fields
+    $acf_fields = SELF::$WPsearch_acf;
+    // for each tag
+    foreach( $exploded as $tag ) :
+      $where .= "
+        AND (
+          (wp_posts.post_title LIKE '%$tag%')
+          OR (wp_posts.post_content LIKE '%$tag%')
+          OR EXISTS (
+            SELECT * FROM wp_postmeta
+              WHERE post_id = wp_posts.ID
+                AND (";
+      // for each acf
+      foreach ($acf_fields as $searcheable_acf) :
+        if ($searcheable_acf == $acf_fields[0]):
+          $where .= " (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
+        else :
+          $where .= " OR (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
+        endif;
+      endforeach;
+      // define where to search
+      $where .= ")
+        )
+        OR EXISTS (
+          SELECT * FROM wp_comments
+          WHERE comment_post_ID = wp_posts.ID
+            AND comment_content LIKE '%$tag%'
+        )
+        OR EXISTS (
+          SELECT * FROM wp_terms
+          INNER JOIN wp_term_taxonomy
+            ON wp_term_taxonomy.term_id = wp_terms.term_id
+          INNER JOIN wp_term_relationships
+            ON wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id
+          WHERE (
+      		taxonomy = 'post_tag'
+        		OR taxonomy = 'category'
+        		OR taxonomy = 'mediathek_sl'
+            OR taxonomy = 'fallbeispiele_s_wk_sm'
+            OR taxonomy = 'fallbeispiele_anlagentyp'
+            OR taxonomy = 'fallbeispiele_sanierungsart'
+            OR taxonomy = 'fallbeispiele_massnahmentyp'
+            OR taxonomy = 'fallbeispiele_fischregion'
+      		)
+          	AND object_id = wp_posts.ID
+          	AND wp_terms.name LIKE '%$tag%'
+        )
+    )";
+  endforeach;
+  // return filter
+  return $where;
+  }
+
+
+
+  /*==================================================================================
+    3.0 OUTPUT
+  ==================================================================================*/
+}
---
+++ classes/prefix_WPsearch/readme.md
@@ -0,0 +1,22 @@
+**Version 1.0** (05.03.2020)
+
+Custom class "prefix_WPsearch" to improve search query.
+
+## CONFIGURATION OPTIONS
+* $WPsearch_acf: list of advanced custom fields you want to search content in
+* $WPsearch_tax: list of taxonomies you want to search content in
+
+## CONFIGURATION FILE
+```
+"WPsearch": {
+  "acf": {
+    "0" => "ACF FIELD NAME"
+  },
+  "taxonomies": {
+    "0" => "TAXONOMY SLUG"
+  }
+}
+```
+
+## USAGE
+Add all ACF field names and taxonomy slugs
